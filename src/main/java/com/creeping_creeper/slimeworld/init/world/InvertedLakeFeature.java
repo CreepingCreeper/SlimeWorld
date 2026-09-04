bf613f0b14805189a4b340afc408cb79072f378a
CreepingCreeper <2714328217@qq.com>
2026-09-04 22:01:11 +0800

    优化import

--- src/main/java/com/creeping_creeper/slimeworld/init/world/InvertedLakeFeature.java
+++ src/main/java/com/creeping_creeper/slimeworld/init/world/InvertedLakeFeature.java
@@ -1,18 +1,14 @@
 package com.creeping_creeper.slimeworld.init.world;
 
 import com.mojang.serialization.Codec;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.BlockPos;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.LakeFeature;
-import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
-import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 
 public class InvertedLakeFeature extends LakeFeature {
     private static final BlockState AIR = Blocks.CAVE_AIR.defaultBlockState();
